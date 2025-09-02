9911cbe7ea26d30ab51dc9b02847f050c842ca7a
bryan karta <karta.bryan@gmail.com>
2025-09-02 11:20:43 +0700

    day1-day3

---
+++ module-01/day-01/excercise.js
@@ -0,0 +1,4 @@
+// 5
+let date1 = new Date("2025-01-01");
+let date2 = new Date("2025-01-31");
+console.log(date2 - date1);
---
+++ module-01/day-03/git.js
@@ -0,0 +1,6 @@
+// VERSI 3
+
+console.log("hello world");
+console.log("semangat");
+console.log("yuk bisa");
+console.log("ini final final final");

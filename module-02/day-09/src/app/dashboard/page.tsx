d8dc4150b5d9a2390887c804ad850d56c015d24c
bryan karta <karta.bryan@gmail.com>
2025-10-15 11:41:52 +0700

    example request be

--- module-02/day-09/src/app/dashboard/page.tsx
+++ module-02/day-09/src/app/dashboard/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function dashboard() {
-  return <div>test</div>;
+  const [data, setData] = useState<number[]>();
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/data` as string
+      );
+
+      setData(data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {data?.map((i, idx) => (
+        <div key={idx}>{i}</div>
+      ))}
+    </div>
+  );
 }
